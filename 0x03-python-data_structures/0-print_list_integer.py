6d65de0413ecad935b6c8528a8a1e569715b14f6
root <root@Y4x.localdomain>
2024-01-09 11:06:31 +0100

    add first projet

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for x in my_list:
-        print(x)
+        print("{}".format(x))
+
+# Test the function
+my_list = [1, 2, 3, 4, 5]
+print_list_integer(my_list)
