f676788cc6abd7df87b89ee99f4da9b7cd20b6f4
root <root@Y4x.localdomain>
2024-01-29 20:27:02 +0100

    fix 0

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-Rectangle = __import__('9-rectangle').Rectangle
+"""Class Rectangle"""
 
-my_square = Rectangle.square(5)
-print("Area: {} - Perimeter: {}".format(my_square.area(), my_square.perimeter()))
-print(my_square)
+
+class Rectangle:
+    """empty class"""
+    pass
