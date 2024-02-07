a21554d8eb42968bf4cedec08146528fa9725084
root <root@Y4x.localdomain>
2024-02-07 01:07:25 +0100

    add 101

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""docs"""
+
+
+def add_attribute(obj, attribute, value):
+    """docs"""
+
+    if hasattr(obj, '__dict__'):
+        if not hasattr(obj, attribute):
+            setattr(obj, attribute, value)
+        else:
+            raise TypeError("can't add new attribute")
+    else:
+        raise TypeError("can't add new attribute")
+    
