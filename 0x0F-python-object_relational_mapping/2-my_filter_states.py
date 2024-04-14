10cc166503d54b216c6a84fae950d9ce90116a87
root <root@Y4x>
2024-04-14 22:32:35 +0100

    add 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument.
+"""
+
+
+import MySQLdb
+from sys import argv, exit
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states where name LIKE '{}'".format(argv[4]))
+    rows = cur.fetchall()
+
+    for data in rows:
+        print(data)
+    cur.close()
+    db.close()
