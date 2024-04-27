29ad12fd2b3d4dc8fe28fb3fa75fae82a2717354
root <root@Y4x>
2024-04-27 16:11:47 +0100

    fix 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-
-# Check if the correct number of arguments is provided
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Send a request using curl and retrieve the size of the body in bytes
-size=$(curl -s -o /dev/null -w "%{size_download}" "$1")
-
-# Display the size of the body
-echo "$size"
+# sends a request to URL and displays the size of the body
+curl -s "$1" | wc -c
