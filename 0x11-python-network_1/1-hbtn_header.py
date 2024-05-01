5c01ebdd10f2dce05e08c1e74b8cfc56ee0d90cd
root <root@Y4x>
2024-05-01 04:04:21 +0100

    fix

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-""" use method .info() to get the headers(response object)"""
-from urllib import request
-from sys import argv
+""" a Python script that takes in a URL.
+- sends a request to the URL
+- displays the value of the X-Request-Id variable found
+in the header of the response.
+"""
 
 
 if __name__ == "__main__":
-    with request.urlopen(argv[1]) as response:
-        headers = response.info()
-        x_request_id = headers.get('X-Request-Id')
-        print(x_request_id)
-        
+    import urllib.request
+    import sys
+
+    with urllib.request.urlopen(sys.argv[1]) as res:
+        req_id = res.getheader("X-Request-Id")
+        print("{}".format(req_id))
+    
