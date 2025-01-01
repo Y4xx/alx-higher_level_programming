7a2e772bfa8a703e7f4455da56c14658de018c9c
Yassine OUJAMA <yassine.oujama@gmail.com>
2025-01-01 15:52:53 +0100

    fix 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,25 +1,11 @@
 #!/usr/bin/python3
-"""
-displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument.
-"""
+""" lists states hbtn_0e_0_usa """
 
-
+import sys
 import MySQLdb
-from sys import argv, exit
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=argv[1], passwd=argv[2], db=argv[3])
-
-    cur = db.cursor()
-
-    cur.execute("SELECT * FROM states where name LIKE '{}'".format(argv[4]))
-    rows = cur.fetchall()
-
-    state_name = argv[4]
-    for data in rows:
-        if (state_name == data[1]):
-            print(data)
-    cur.close()
-    db.close()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall() if state[1] == sys.argv[4]]
