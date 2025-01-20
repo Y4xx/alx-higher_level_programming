6960c13a39f4ebbbf695f40f50f45da3769892e9
Yassine OUJAMA <yassine.oujama@gmail.com>
2025-01-21 00:32:44 +0100

    fix 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""script that fetches URL"""
-from requests import get
+"""python script"""
+import requests
 
 
 if __name__ == "__main__":
-    response = get("https://alx-intranet.hbtn.io/status")
+    r = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
